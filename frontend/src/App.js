1b37e5e2c55140a91000f6b8538b2cad7d415a08
Edward <edward42105@gmail.com>
2025-02-01 18:00:00 -0500

    merge app function to one

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Filter from './features/filter';
-//Searchbar
 import React, { useState } from "react";
 import SearchBar from "./SearchBar";
 import "./SearchBar.css";
 
 const App = () => {
     const [searchQuery, setSearchQuery] = useState("");
+    const [selectedGenres, setSelectedGenres] = useState([]);
 
     const handleSearch = (query) => {
         setSearchQuery(query);
@@ -16,34 +16,25 @@ const App = () => {
     return (
         <div>
             <h1>Game Search Engine</h1>
+            
+            {/* Search bar */}
             <SearchBar onSearch={handleSearch} />
             <p>Search term: {searchQuery}</p>
+            
+            {/* Filter component */}
+            <Filter selectedGenres={selectedGenres} setSelectedGenres={setSelectedGenres} />
+            
+            {/* Display selected genres */}
+            <div>
+                <h2>Selected Genres:</h2>
+                <ul>
+                    {selectedGenres.map((genre) => (
+                        <li key={genre}>{genre}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 };
-
-
-function App() {
-  const [selectedGenres, setSelectedGenres] = useState([]);
-
-  return (
-    <div>
-      <h1>Game Search</h1>
-      
-      {/* Use Filter component */}
-      <Filter selectedGenres={selectedGenres} setSelectedGenres={setSelectedGenres} />
-      
-      {/* Display selected genres */}
-      <div>
-        <h2>Selected Genres:</h2>
-        <ul>
-          {selectedGenres.map((genre) => (
-            <li key={genre}>{genre}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-}
 
 export default App;
