caafbc994acf5019763c1faee0b62f58678a64e1
Edward <edward42105@gmail.com>
2025-02-01 17:14:17 -0500

    filter changes

---
+++ frontend/src/features/filter.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+
+function Filter() {
+    const [selectedFilters, setSelectedFilters] = useState([]);
+}
+
+const handleCheckboxChange = (event) => {
+    const { value, checked } = event.target;
+  
+    if (checked) {
+      // Add the filter to the selected filters
+      setSelectedFilters((prevFilters) => [...prevFilters, value]);
+    } else {
+      // Remove the filter from the selected filters
+      setSelectedFilters((prevFilters) => prevFilters.filter((filter) => filter !== value));
+    }
+  };
+
+  return (
+    <div>
+      <h3>Filter Games by Genre</h3>
+      
+      {/* Step 2: Create checkboxes for different filters */}
+      <label>
+        <input
+          type="checkbox"
+          value="Action"
+          onChange={handleCheckboxChange}
+        />
+        Action
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          value="Adventure"
+          onChange={handleCheckboxChange}
+        />
+        Adventure
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          value="RPG"
+          onChange={handleCheckboxChange}
+        />
+        RPG
+      </label>
+
+      {/* Step 4: Display selected filters */}
+      <div>
+        <p>Selected Filters:</p>
+        <ul>
+          {selectedFilters.map((filter, index) => (
+            <li key={index}>{filter}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+
+  export default Filter;
