dfd8d4cb303c70c625fb45a52bb2e9a8e57a847f
mshen5390 <140674495+mshen5390@users.noreply.github.com>
2025-02-02 05:53:34 -0500

    revert

--- frontend/src/features/filter.js
+++ frontend/src/features/filter.js
@@ -1,18 +1,57 @@
 import React, { useState } from 'react';
 
 function Filter() {
-  const Filter = ({ selectedGenres, setSelectedGenres }) => {
+  const [selectedFilters, setSelectedFilters] = useState([]);
+    const handleCheckboxChange = (event) => {
+      const { value, checked } = event.target;
+    
+      if (checked) {
+        // Add the filter to the selected filters
+        setSelectedFilters((prevFilters) => [...prevFilters, value]);
+      } else {
+        // Remove the filter from the selected filters
+        setSelectedFilters((prevFilters) => prevFilters.filter((filter) => filter !== value));
+      }
+    };
     return (
       <div>
-        <h3>Selected Genres</h3>
-        <ul>
-          {selectedGenres.map((genre) => (
-            <li key={genre.id}>{genre}</li>
-          ))}
-        </ul>
+        <h3>Filter Games by Genre</h3>
+        
+        {/* Create checkboxes for different filters */}
+        <label>
+          <input
+            type="checkbox"
+            value="Action"
+            onChange={handleCheckboxChange}
+          />
+          Action
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            value="Adventure"
+            onChange={handleCheckboxChange}
+          />
+          Adventure
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            value="RPG"
+            onChange={handleCheckboxChange}
+          />
+          RPG
+        </label>
+        {/* Display selected filters */}
+      <div>
+          <p>Selected Filters:</p>
+            <ul>
+            {selectedFilters.map((filter, index) => (
+              <li key={index}>{filter}</li>
+            ))}
+            </ul>
+        </div>
       </div>
     );
-  };
 }
-
 export default Filter;
